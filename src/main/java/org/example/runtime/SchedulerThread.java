88f3f94755d22539332089fdfb4693813a621264
mkhoshechin <mohammad.khoshechin76@gmail.com>
2024-02-28 14:49:08 +0100

    Fixed a bug in SchedulerThread

--- src/main/java/org/example/runtime/SchedulerThread.java
+++ src/main/java/org/example/runtime/SchedulerThread.java
@@ -102,8 +102,11 @@ public class SchedulerThread extends Thread{
         System.out.println("**********************************************************************************************");
         System.out.println("[*** The SchedulerThread requested to FINISH***]");
         System.out.println("**********************************************************************************************");
-        System.exit(0);
+        //System.exit(0);
 
+        synchronized (RuntimeEnvironment.locks.get((long) 1)){
+            RuntimeEnvironment.locks.get((long) 1).notify();
+        }
     }
 
     /*
