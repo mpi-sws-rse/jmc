8fe298a2c98f62f925dec4b796c5d307c6b0a941
Srinidhi Nagendra <srinidhin@outlook.com>
2025-04-09 11:32:53 +0200

    Checkstyle

--- agent/src/main/java/org/mpisws/jmc/agent/visitors/JmcFutureVisitor.java
+++ agent/src/main/java/org/mpisws/jmc/agent/visitors/JmcFutureVisitor.java
@@ -1,6 +1,8 @@
 package org.mpisws.jmc.agent.visitors;
 
-import org.objectweb.asm.*;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
 import java.util.HashMap;
 import java.util.Set;
