cb092dea22f4eb6ad301074fdc48f295bd63708e
Srinidhi Nagendra <hello@srinidhin.com>
2025-04-28 23:24:03 +0200

    Make tests parametric

--- core/src/test/java/org/mpisws/jmc/checker/JmcCoverageBenchmark.java
+++ core/src/test/java/org/mpisws/jmc/checker/JmcCoverageBenchmark.java
@@ -1,6 +1,8 @@
 package org.mpisws.jmc.checker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mpisws.jmc.checker.exceptions.JmcCheckerException;
 import org.mpisws.jmc.checker.exceptions.JmcCheckerTimeoutException;
 import org.mpisws.jmc.programs.concurrent.CC7;
@@ -11,93 +13,97 @@ import org.mpisws.jmc.strategies.trust.TrustStrategy;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class JmcCoverageBenchmark {
-    @Test
-    public void benchmarkCorrectCounterTrust() throws JmcCheckerException {
-        HashMap<Integer, Duration> timeoutMap = new HashMap<>();
-        timeoutMap.put(6, Duration.of(3, ChronoUnit.MINUTES));
-        timeoutMap.put(7, Duration.of(10, ChronoUnit.MINUTES));
-        timeoutMap.put(8, Duration.of(15, ChronoUnit.MINUTES));
-        for (int i = 6; i < 9; i++) {
-            int localI = i;
-            System.out.println("Running with i = " + i);
-            JmcCheckerConfiguration config =
-                    new JmcCheckerConfiguration.Builder()
-                            .strategyConstructor(
-                                    (sConfig) ->
-                                            new MeasureGraphCoverageStrategy(
-                                                    new TrustStrategy(
-                                                            sConfig.getSeed(),
-                                                            sConfig.getTrustSchedulingPolicy(),
-                                                            sConfig.getDebug(),
-                                                            sConfig.getReportPath()),
-                                                    false,
-                                                    sConfig.getReportPath() + "/trust-" + localI,
-                                                    false,
-                                                    Duration.of(5, ChronoUnit.MILLIS)))
-                            .timeout(timeoutMap.get(localI))
-                            .debug(false)
-                            .build();
-            JmcModelChecker jmcModelChecker = new JmcModelChecker(config);
 
-            JmcTestTarget target =
-                    new JmcFunctionalTestTarget(
-                            "TrustCC1",
-                            () -> {
-                                CC7.main(new String[] {String.valueOf(localI)});
-                            });
-            try {
-                jmcModelChecker.check(target);
-            } catch (JmcCheckerTimeoutException ignored) {
+    private static Stream<Arguments> provideTrustTestCases() {
+        return Stream.of(
+                Arguments.of(6, Duration.of(6, ChronoUnit.MINUTES)),
+                Arguments.of(7, Duration.of(10, ChronoUnit.MINUTES)));
+    }
 
-            } catch (JmcCheckerException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
+    private static Stream<Arguments> provideRandomTestCases() {
+        return Stream.of(
+                Arguments.of(6, Duration.of(6, ChronoUnit.MINUTES)),
+                Arguments.of(6, Duration.of(12, ChronoUnit.MINUTES)),
+                Arguments.of(7, Duration.of(10, ChronoUnit.MINUTES)),
+                Arguments.of(7, Duration.of(20, ChronoUnit.MINUTES)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTrustTestCases")
+    public void benchmarkCorrectCounterTrust(int threads, Duration timeout)
+            throws JmcCheckerException {
+        System.out.println("Running with threads = " + threads);
+        JmcCheckerConfiguration config =
+                new JmcCheckerConfiguration.Builder()
+                        .strategyConstructor(
+                                (sConfig) ->
+                                        new MeasureGraphCoverageStrategy(
+                                                new TrustStrategy(
+                                                        sConfig.getSeed(),
+                                                        sConfig.getTrustSchedulingPolicy(),
+                                                        sConfig.getDebug(),
+                                                        sConfig.getReportPath()),
+                                                false,
+                                                sConfig.getReportPath() + "/trust-" + threads,
+                                                false,
+                                                Duration.of(5, ChronoUnit.MILLIS)))
+                        .timeout(timeout)
+                        .debug(false)
+                        .build();
+        JmcModelChecker jmcModelChecker = new JmcModelChecker(config);
+
+        JmcTestTarget target =
+                new JmcFunctionalTestTarget(
+                        "TrustCC1",
+                        () -> {
+                            CC7.main(new String[] {String.valueOf(threads)});
+                        });
+        try {
+            jmcModelChecker.check(target);
+        } catch (JmcCheckerTimeoutException ignored) {
+
+        } catch (JmcCheckerException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
-    @Test
-    public void benchmarkCorrectCounterRandom() throws JmcCheckerException {
-        HashMap<Integer, Duration> timeoutMap = new HashMap<>();
-        timeoutMap.put(6, Duration.of(3, ChronoUnit.MINUTES));
-        timeoutMap.put(7, Duration.of(10, ChronoUnit.MINUTES));
-        timeoutMap.put(8, Duration.of(15, ChronoUnit.MINUTES));
-        for (int i = 6; i < 9; i++) {
-            int localI = i;
-            System.out.println("Running with i = " + i);
-            // Random math to determine the number of iterations
-            Duration timeout = timeoutMap.get(localI);
-            JmcCheckerConfiguration config =
-                    new JmcCheckerConfiguration.Builder()
-                            .strategyConstructor(
-                                    (sConfig) ->
-                                            new MeasureGraphCoverageStrategy(
-                                                    new RandomSchedulingStrategy(sConfig.getSeed()),
-                                                    false,
-                                                    sConfig.getReportPath()
-                                                            + "/random-"
-                                                            + localI
-                                                            + "-"
-                                                            + timeout.toString(),
-                                                    true,
-                                                    Duration.of(5, ChronoUnit.MILLIS)))
-                            .timeout(timeout)
-                            .debug(false)
-                            .build();
-            JmcModelChecker jmcModelChecker = new JmcModelChecker(config);
+    @ParameterizedTest
+    @MethodSource("provideRandomTestCases")
+    public void benchmarkCorrectCounterRandom(int threads, Duration timeout)
+            throws JmcCheckerException {
+        System.out.println("Running with threads = " + threads);
+        JmcCheckerConfiguration config =
+                new JmcCheckerConfiguration.Builder()
+                        .strategyConstructor(
+                                (sConfig) ->
+                                        new MeasureGraphCoverageStrategy(
+                                                new RandomSchedulingStrategy(sConfig.getSeed()),
+                                                false,
+                                                sConfig.getReportPath()
+                                                        + "/random-"
+                                                        + threads
+                                                        + "-"
+                                                        + timeout.toString(),
+                                                true,
+                                                Duration.of(5, ChronoUnit.MILLIS)))
+                        .timeout(timeout)
+                        .debug(false)
+                        .build();
+        JmcModelChecker jmcModelChecker = new JmcModelChecker(config);
 
-            JmcTestTarget target =
-                    new JmcFunctionalTestTarget(
-                            "TrustCC1",
-                            () -> {
-                                CC7.main(new String[] {String.valueOf(localI)});
-                            });
-            try {
-                jmcModelChecker.check(target);
-            } catch (JmcCheckerException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
+        JmcTestTarget target =
+                new JmcFunctionalTestTarget(
+                        "TrustCC1",
+                        () -> {
+                            CC7.main(new String[] {String.valueOf(threads)});
+                        });
+        try {
+            jmcModelChecker.check(target);
+        } catch (JmcCheckerException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
