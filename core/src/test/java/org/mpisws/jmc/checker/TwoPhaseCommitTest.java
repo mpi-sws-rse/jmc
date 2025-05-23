04bce58c022e2dbe617fe9436120f52086f72430
Mohammad Hossein Khoshechin Jorshari <mkhoshechin@mpi-sws.org>
2025-05-23 15:01:51 +0200

    Updated 2PS test

--- core/src/test/java/org/mpisws/jmc/checker/TwoPhaseCommitTest.java
+++ core/src/test/java/org/mpisws/jmc/checker/TwoPhaseCommitTest.java
@@ -4,19 +4,14 @@ import org.mpisws.jmc.annotations.JmcCheck;
 import org.mpisws.jmc.annotations.JmcCheckConfiguration;
 import org.mpisws.jmc.annotations.JmcTimeout;
 import org.mpisws.jmc.programs.twophasecommit.Coordinator;
+import org.mpisws.jmc.programs.twophasecommit.TwoPhaseCommit;
 
 import java.time.temporal.ChronoUnit;
 
 public class TwoPhaseCommitTest {
     @JmcCheck
-    @JmcCheckConfiguration(numIterations = 2, debug = true, strategy = "trust")
+    @JmcCheckConfiguration(numIterations = 2, debug = false, strategy = "trust")
     void testTPCCommit() {
-        Coordinator coordinator = new Coordinator(2);
-        coordinator.start();
-
-        // Simulate sending a request to the coordinator
-        assert coordinator.acceptRequest(1);
-
-        coordinator.stop();
+        TwoPhaseCommit.main(new String[]{"2"});
     }
 }
---
+++ core/src/test/java/org/mpisws/jmc/programs/twophasecommit/TwoPhaseCommit.java
@@ -0,0 +1,28 @@
+package org.mpisws.jmc.programs.twophasecommit;
+
+import org.mpisws.jmc.util.concurrent.JmcThread;
+
+public class TwoPhaseCommit {
+
+    public static void main(String[] args) {
+        int numParticipants = args.length > 0 ? Integer.parseInt(args[0]) : 2;
+        try {
+            JmcThread tps = new JmcThread(
+                    () -> {
+                        try {
+                            Coordinator coordinator = new Coordinator(numParticipants);
+                            coordinator.start();
+
+                            // Simulate sending a request to the coordinator
+                            assert coordinator.acceptRequest(1);
+
+                            coordinator.stop();
+                        } catch (Exception e) {
+                        }
+                    });
+            tps.start();
+            tps.join1();
+        } catch (InterruptedException e) {
+        }
+    }
+}
