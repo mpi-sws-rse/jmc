d332636a6316af710ffe30d6251c4d81e5618ad9
Srinidhi N <srinidhin@outlook.com>
2025-06-29 22:01:07 +0200

    Updated ReadN test

--- integration-test/src/test/java/org/mpisws/jmc/test/ReadNTest.java
+++ integration-test/src/test/java/org/mpisws/jmc/test/ReadNTest.java
@@ -2,15 +2,32 @@ package org.mpisws.jmc.test;
 
 import org.mpisws.jmc.annotations.JmcCheck;
 import org.mpisws.jmc.annotations.JmcCheckConfiguration;
-import org.mpisws.jmc.test.readN.ReadN;
+import org.mpisws.jmc.test.readN.Shared;
+import org.mpisws.jmc.test.readN.ThreadA;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ReadNTest {
 
+    private void readNProgram() {
+        Shared shared = new Shared(0);
+        List<ThreadA> threads = new ArrayList<ThreadA>();
+        int numThreads = 10;
+        for (int i = 0; i < numThreads; i++) {
+            ThreadA thread = new ThreadA(shared);
+            threads.add(thread);
+        }
+        for (int i = 0; i < numThreads; i++) {
+            threads.get(i).start();
+        }
+    }
+
     // Running with JMC using the trust strategy.
     @JmcCheck
-    @JmcCheckConfiguration(numIterations = 1, debug = true, strategy = "estimation")
+    @JmcCheckConfiguration(numIterations = 1, strategy = "estimation")
     public void runEstimationReadNTest() {
-        ReadN.main(new String[]{});
+        readNProgram();
     }
 }
