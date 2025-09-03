c8d395a7b71bd3331b2e82abd836e9963fcb3707
Tanvi Moharir <tanvimoharir@gmail.com>
2025-09-03 13:53:17 +0200

    Changes for ObjectWaitNotify

--- agent/src/main/java/org/mpi_sws/jmc/agent/visitors/JmcWaitNotifyVisitor.java
+++ agent/src/main/java/org/mpi_sws/jmc/agent/visitors/JmcWaitNotifyVisitor.java
@@ -1,6 +1,7 @@
 package org.mpi_sws.jmc.agent.visitors;
 
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 /**
@@ -13,10 +14,19 @@ import org.objectweb.asm.Opcodes;
  * <p>o.notify() -> JmcObject.objectNotify(o)
  *
  * <p>o.notifyAll() -> JmcObject.objectNotifyAll(o)
+ * check thread visitor for reference
  */
 public class JmcWaitNotifyVisitor extends ClassVisitor {
 
     public JmcWaitNotifyVisitor(ClassVisitor cv) {
         super(Opcodes.ASM9, cv);
     }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+        MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
+
+        return new JmcNotifyWaitMethodVisitor(mv);
+    }
 }
+
---
+++ integration-test/src/test/java/org/mpi_sws/jmc/test/features/ObjectWaitNotifyTest.java
@@ -0,0 +1,42 @@
+package org.mpi_sws.jmc.test.features;
+
+import org.mpi_sws.jmc.annotations.JmcCheck;
+import org.mpi_sws.jmc.annotations.JmcCheckConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ObjectWaitNotifyTest {
+
+    @JmcCheck
+    @JmcCheckConfiguration(numIterations = 10)
+    public void testBasicWaitNotify() throws InterruptedException {
+        final Object obj = new Object();
+        final boolean[] flag = {false};
+
+        Thread waiter = new Thread(() -> {
+            synchronized (obj) {
+                while (!flag[0]) {
+                    try {
+                        obj.wait();
+                    } catch (InterruptedException e) {}
+                }
+            }
+        });
+
+        Thread notifier = new Thread(() -> {
+            synchronized (obj) {
+                flag[0] = true;
+                obj.notify();
+            }
+        });
+
+        waiter.start();
+        Thread.sleep(50); // Ensure waiter calls wait()
+        notifier.start();
+
+        waiter.join();
+        notifier.join();
+
+        assertTrue(flag[0]);
+    }
+}
