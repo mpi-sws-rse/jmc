35506bcfc039c1c0779a5ee26c1892b94ab8d29c
Tanvi Moharir <tanvimoharir@gmail.com>
2025-09-04 15:00:06 +0200

    Tweaking notify visitor

--- agent/src/main/java/org/mpi_sws/jmc/agent/visitors/JmcWaitNotifyVisitor.java
+++ agent/src/main/java/org/mpi_sws/jmc/agent/visitors/JmcWaitNotifyVisitor.java
@@ -1,6 +1,7 @@
 package org.mpi_sws.jmc.agent.visitors;
 
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -18,6 +19,8 @@ import org.objectweb.asm.Opcodes;
  */
 public class JmcWaitNotifyVisitor extends ClassVisitor {
 
+
+
     public JmcWaitNotifyVisitor(ClassVisitor cv) {
         super(Opcodes.ASM9, cv);
     }
@@ -28,5 +31,64 @@ public class JmcWaitNotifyVisitor extends ClassVisitor {
 
         return new JmcNotifyWaitMethodVisitor(mv);
     }
+
+
+
+    public static class JmcNotifyWaitMethodVisitor  extends MethodVisitor {
+
+        public JmcNotifyWaitMethodVisitor(MethodVisitor mv) {
+            super(Opcodes.ASM9, mv);
+        }
+
+        @Override
+        public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
+            switch (name) {
+                case "wait":
+                    if (descriptor.equals("()V")) {
+                        super.visitMethodInsn(
+                                Opcodes.INVOKESTATIC,
+                                "org/mpi_sws/jmc/api/JmcObject",
+                                "objectWait",
+                                "(Ljava/lang/Object;)V",
+                                false
+                        );
+                        return;
+                    } else if (descriptor.equals("(J)V")) {
+                        super.visitMethodInsn(
+                                Opcodes.INVOKESTATIC,
+                                "org/mpi_sws/jmc/api/JmcObject",
+                                "objectWait",
+                                "(Ljava/lang/Object;J)V",
+                                false
+                        );
+                        return;
+                    }
+                    break;
+                case "notify":
+                    super.visitMethodInsn(
+                            Opcodes.INVOKESTATIC,
+                            "org/mpi_sws/jmc/api/JmcObject",
+                            "objectNotify",
+                            "(Ljava/lang/Object;)V",
+                            false
+                    );
+                    return;
+                case "notifyAll":
+                    super.visitMethodInsn(
+                            Opcodes.INVOKESTATIC,
+                            "org/mpi_sws/jmc/api/JmcObject",
+                            "objectNotifyAll",
+                            "(Ljava/lang/Object;)V",
+                            false
+                    );
+                    return;
+                default:
+                    super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+            }
+
+        }
+    }
+
+
 }
 
