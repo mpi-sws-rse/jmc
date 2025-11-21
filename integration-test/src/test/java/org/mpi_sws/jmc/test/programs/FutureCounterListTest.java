82681676879a620d8e969c21e3dc1e9d7d0d5bb3
Mohammad Hossein Khoshechin Jorshari <mkhoshechin@mpi-sws.org>
2025-11-21 18:48:11 +0100

    updated tests

--- integration-test/src/test/java/org/mpi_sws/jmc/test/programs/FutureCounterListTest.java
+++ integration-test/src/test/java/org/mpi_sws/jmc/test/programs/FutureCounterListTest.java
@@ -5,43 +5,45 @@ import org.mpi_sws.jmc.test.FutureTaskCounter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FutureCounterListTest {
-    public static void main(String[] args) throws Exception {
-    AtomicInteger counter = new AtomicInteger();
-    ExecutorService executor = Executors.newFixedThreadPool(2);
-    //ExecutorService executor = Executors.newSingleThreadExecutor();
-    List<Future<Integer>> futures = new ArrayList<>();
+    public static void main(String[] args) {
+        AtomicInteger counter = new AtomicInteger();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        //ExecutorService executor = Executors.newSingleThreadExecutor();
+        List<Future<Integer>> futures = new ArrayList<>();
 
-    for (int i = 0; i < 3; i++) {
-        Callable<Integer> callable = new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                counter.incrementAndGet();
-                return 1;
+        for (int i = 0; i < 3; i++) {
+            Callable<Integer> callable = new Callable<Integer>() {
+                @Override
+                public Integer call() throws Exception {
+                    counter.incrementAndGet();
+                    return 1;
+                }
+            };
+
+            Future<Integer> future = executor.submit(callable);
+            System.out.println(" Returned Future implementation: " + future.getClass().getName());
+            futures.add(future);
+        }
+
+        for (Future<Integer> future : futures) {
+            System.out.println("Waiting for future: " + future);
+            try {
+                future.get();
+            } catch (InterruptedException e) {
+
+            } catch (ExecutionException e) {
+
             }
-        };
-
-        Future<Integer> future = executor.submit(callable);
-        System.out.println(" Returned Future implementation: " + future.getClass().getName());
-        futures.add(future);
-    }
-
-    for (Future<Integer> future : futures) {
-        System.out.println("Waiting for future: " + future);
-        future.get();
-        System.out.println("Finished future.get");
-    }
-    assertEquals(3, counter.get());
-
+            System.out.println("Finished future.get");
+        }
+        assertEquals(3, counter.get());
 
 
     }
