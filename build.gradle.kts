fa5f2cd70449cd1b256fc237d32ae3e4a08f3aae
Mohammad Hossein Khoshechin Jorshari <mkhoshechin@mpi-sws.org>
2026-06-09 11:08:23 +0200

    [Gradle] Fixed a vulnerability issue with Apache Commons Lang

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ allprojects {
     //  - plexus-utils 3.3.0: Directory Traversal in Expand.extractFile (< 3.6.1)
     //  - commons-beanutils 1.9.4: Improper Access Control / RCE via enum
     //    declaredClass (<= 1.10.1, patched in 1.11.0)
+    //  - commons-lang3 3.8.1: Uncontrolled Recursion / StackOverflowError in
+    //    ClassUtils.getClass on long inputs (< 3.18.0)
     // Applies only to modules that use Checkstyle. Remove if/when the Checkstyle
     // toolVersion is bumped to a release that no longer ships these vulnerable versions.
     plugins.withId("checkstyle") {
@@ -38,6 +40,7 @@ allprojects {
             resolutionStrategy {
                 force("org.codehaus.plexus:plexus-utils:3.6.1")
                 force("commons-beanutils:commons-beanutils:1.11.0")
+                force("org.apache.commons:commons-lang3:3.18.0")
             }
         }
     }
